3357e5526c01469d0464eb34b626621edc2abcba
mdmubin <mdmubin.xyz@gmail.com>
2025-09-09 13:16:46 +0100

    Updated documentations.

--- include/quartz/types.hpp
+++ include/quartz/types.hpp
@@ -4,54 +4,55 @@ namespace qz
 {
 
 /**
-* @typedef s8
+ * @defgroup QzTypes Fixed-width types
+ * @brief Integer and floating point typedefs used in the qz namespace.
+ *
+ * @{
+ */
+
+/**
 * @brief Signed 8-bit integer type.
 */
 using s8 = signed char;
 /**
-* @typedef s16
 * @brief Signed 16-bit integer type.
 */
 using s16 = short;
 /**
-* @typedef s32
 * @brief Signed 32-bit integer type.
 */
 using s32 = int;
 /**
-* @typedef s64
 * @brief Signed 64-bit integer type.
 */
 using s64 = long long;
 /**
-* @typedef u8
-* @brief Signed 8-bit integer type.
+* @brief Unsigned 8-bit integer type.
 */
 using u8 = unsigned char;
 /**
-* @typedef u16
-* @brief Signed 16-bit integer type.
+* @brief Unsigned 16-bit integer type.
 */
 using u16 = unsigned short;
 /**
-* @typedef u32
-* @brief Signed 32-bit integer type.
+* @brief Unsigned 32-bit integer type.
 */
 using u32 = unsigned int;
 /**
-* @typedef u64
-* @brief Signed 64-bit integer type.
+* @brief Unsigned 64-bit integer type.
 */
 using u64 = unsigned long long;
 /**
-* @typedef f32
 * @brief 32-bit floating point type.
 */
 using f32 = float;
 /**
-* @typedef f64
 * @brief 64-bit floating point type.
 */
 using f64 = double;
+
+/**
+ * @}
+ */
 
 } // namespace qz
