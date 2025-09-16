66e14c47c4e27fcf6ec6d4d79759570735e80ab3
mdmubin <mdmubin.xyz@gmail.com>
2025-09-16 11:33:35 +0100

    Added new identify helper macros to `macros.hpp`.

--- include/quartz/macros.hpp
+++ include/quartz/macros.hpp
@@ -6,6 +6,87 @@
 /// @{
 ///
 
+#if defined(__linux) || defined(__linux__)
+    /// @ingroup QzPlatformMacros
+    /// @brief Defined if the current platform is linux.
+    #define QZ_PLATFORM_LINUX 1
+    /// @ingroup QzPlatformMacros
+    /// @brief Platform name defined if the current platform is linux.
+    #define QZ_PLATFORM_STRING "linux"
+#elif defined(_WIN32) || defined(_WIN64)
+    /// @ingroup QzPlatformMacros
+    /// @brief Defined if the current platform is windows.
+    #define QZ_PLATFORM_WINDOWS 1
+    /// @ingroup QzPlatformMacros
+    /// @brief Platform name defined if the current platform is windows.
+    #define QZ_PLATFORM_STRING "windows"
+#else
+    #error unrecognized platform
+#endif
+
+#if defined(QZ_PLATFORM_LINUX)
+    #if defined(__clang__)
+        #define QZ_COMPILER_CLANG 1
+        #define QZ_COMPILER_STRING "clang"
+        #define QZ_COMPILER_VERSION (__clang_major__ * 100 + __clang_minor__)
+    #elif defined(__GNUC__)
+        #define QZ_COMPILER_GCC 1
+        #define QZ_COMPILER_STRING "gcc"
+        #define QZ_COMPILER_VERSION (__GNUC__ * 100 + __GNUC_MINOR__)
+    #else
+        #error unrecognized compiler.
+    #endif
+#elif defined(QZ_PLATFORM_WINDOWS)
+    #if defined(__clang__)
+        #define QZ_COMPILER_CLANG 1
+        #if defined(_MSC_VER)
+            #define QZ_COMPILER_CLANG_CL 1
+            #define QZ_COMPILER_STRING "clang-cl"
+        #elif defined(__MINGW32__) || defined(__MINGW64__)
+            #define QZ_COMPILER_MINGW_CLANG 1
+            #define QZ_COMPILER_STRING "mingw-clang"
+        #else
+            #define QZ_COMPILER_STRING "clang"
+        #endif
+        #define QZ_COMPILER_VERSION (__clang_major__ * 100 + __clang_minor__)
+    #elif defined(_MSC_VER)
+        #define QZ_COMPILER_MSVC 1
+        #define QZ_COMPILER_STRING "msvc"
+        #define QZ_COMPILER_VERSION _MSC_VER
+    #elif defined(__GNUC__)
+        #define QZ_COMPILER_GCC 1
+        #if defined(__MINGW32__) || defined(__MINGW64__)
+            #define QZ_COMPILER_MINGW_GCC 1
+            #define QZ_COMPILER_STRING "mingw-gcc"
+        #else
+            #define QZ_COMPILER_STRING "gcc"
+        #endif
+        #define QZ_COMPILER_VERSION (__GNUC__ * 100 + __GNUC_MINOR__)
+    #else
+        #error unrecognized compiler.
+    #endif
+#else
+    #error cannot determine compiler on unrecognized platform.
+#endif
+
+#if defined(QZ_PLATFORM_LINUX)
+    #if defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(__amd64__)
+        #define QZ_ARCHITECTURE_X64 1
+        #define QZ_ARCHITECTURE_STRING "x64"
+    #else
+        #error unrecognized platform architecture.
+    #endif
+#elif defined(QZ_PLATFORM_WINDOWS)
+    #if defined(_M_X64) || defined(_M_AMD64)
+        #define QZ_ARCHITECTURE_X64 1
+        #define QZ_ARCHITECTURE_STRING "x64"
+    #else
+        #error unrecognized platform architecture.
+    #endif
+#else
+    #error cannot determine architecture on unrecognized platform.
+#endif
+
 ///
 /// @defgroup QzDebugMacros Debug helper macros
 /// @ingroup QzMacros
@@ -64,6 +145,32 @@
 /// @brief Convert the arguments to a single wide char string.
 ///
 #define QZ_STRINGIFY_W(...) QZ_CONCAT(L, QZ_STRINGIFY(__VA_ARGS__))
+
+///
+/// @ingroup QzUtilityMacros
+/// @brief Check if the compiler has a certain builtin function.
+///
+#if defined(__has_builtin)
+    #define QZ_HAS_BUILTIN(X) __has_builtin(X)
+#else
+    #define QZ_HAS_BUILTIN(X) 0
+#endif
+
+///
+/// @ingroup QzUtilityMacros
+/// @brief Mark a certain branch of code as unreachable/unlikely to be reached.
+///
+#if defined(QZ_COMPILER_MSVC)
+    #define QZ_UNREACHABLE() __assume(false)
+#elif QZ_HAS_BUILTIN(__builtin_unreachable)
+    #define QZ_UNREACHABLE() __builtin_unreachable()
+#endif
+
+///
+/// @ingroup QzUtilityMacros
+/// @brief Mark a certain variable or statement as unused.
+///
+#define QZ_UNUSED(X) static_cast<void>(X)
 
 ///
 /// @}
